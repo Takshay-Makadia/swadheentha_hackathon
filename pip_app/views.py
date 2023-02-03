2e86c658622d4f99ec92d58a7ae0e4af5e9729bd
Isha Shah <ishashahjan@gmail.com>
2023-02-04 03:27:21 +0530

    navbar added

--- pip_app/views.py
+++ pip_app/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def home(request):
     return HttpResponse("Hello, Django!")
+def navbar(request):
+    template = loader.get_template('navbar.html')
+    context={
+
+    }
+    return HttpResponse(template.render(context,request))
